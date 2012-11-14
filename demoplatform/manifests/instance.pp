46232c1e46456e27289c27e036bd0b0039c154be
thallgren <thomas@tada.se>
2012-11-14 10:56:28 +0100

    Removed lint errors and bumped versions

--- demoplatform/manifests/instance.pp
+++ demoplatform/manifests/instance.pp
@@ -5,12 +5,12 @@
 define demoplatform::instance($instance_location = '/tmp') {
   include demoplatform::internal::prerequisites
   $instance_alias = $name
-	
+
   exec { "demo_download-${instance_alias}":
     command => "curl -sS \"https://raw.github.com/cloudsmith/public-puppetmodules/master/demoplatform/README\" > ${instance_location}/README-${instance_alias}",
     path => ['/usr/local/bin', '/bin', '/usr/bin'],
     creates => "${instance_location}/README-${instance_alias}",
     require => Package['curl'],
   }
- 
+
 }
